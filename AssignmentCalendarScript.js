6b2bba1d14b3d9acbd85546c91b630c0dc84ced6
SamKhan32 <106261217+SamKhan32@users.noreply.github.com>
2023-11-10 13:42:27 -0500

    Updated js

--- AssignmentCalendarScript.js
+++ AssignmentCalendarScript.js
@@ -2,9 +2,27 @@
 
 // Grabs the JSON object from the canvas
 
-function jsonGrabber(id) {
+        function jsonGrabber(fileInput) {
+            // Check if a file is selected
+            if (fileInput.files.length > 0) {
+                // Get the selected file
+                var selectedFile = fileInput.files[0];
 
-	
-	let stream = prompt("PUT IN THE EVENTSTREAM HERE");
+                // Create a FileReader to read the file
+                var reader = new FileReader();
 
-}
+                // Set up the FileReader onload event
+                reader.onload = function(event) {
+                    // The content of the file is available in event.target.result
+                    var fileContent = event.target.result;
+
+                    // You can do something with the file content here
+                    console.log('File content:', fileContent);
+                };
+
+                // Read the file as text
+                reader.readAsText(selectedFile);
+            } else {
+                console.log('No file selected.');
+            }
+        }
